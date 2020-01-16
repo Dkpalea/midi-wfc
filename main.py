56736e0c97d3acd80a450bd2b83bcc0141d4f01f
Dkpalea <dkpalea@ucsc.edu>
2020-01-16 13:14:05 -0800

    output images

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pretty_midi
 import math
+import random
+from PIL import Image, ImageDraw
 
 # Settings ----------------
 
 # length in secs
-audio_len = 5
+audio_len = 50
 # resolution in hz
-audio_res = 5
+audio_res = 50
 
 # output dimensions = (len*res)^2
 
 # Settings ----------------
 
 
-midi_data = pretty_midi.PrettyMIDI('midi_downloads/mario.mid')
+midi_data = pretty_midi.PrettyMIDI('midi_downloads/africa.mid')
 num_inst = len(midi_data.instruments)
 
 def discretize(input_time):
@@ -31,14 +33,20 @@ def discretize(input_time):
 
 print(discretize(1.75))
 
+img = Image.new('RGB', (audio_len*audio_res, 25), 'black')
+idraw = ImageDraw.Draw(img)
+
 for inst_index in range (0, num_inst):
     instrument = midi_data.instruments[inst_index]
     print("inst"+str(inst_index))
     for note_index in range (0, len(instrument.notes) - 1):
         if (instrument.notes[note_index].start< audio_len):
             print([instrument.notes[note_index]])
+            idraw.line([(discretize(instrument.notes[note_index].start), inst_index),\
+                        (discretize(instrument.notes[note_index].end), inst_index)],\
+                       (instrument.notes[note_index].pitch,random.randrange(0,255),255)\
+                       )
         else:
             break
 
-
-# print(midi_data.instruments[0].notes)
+img.save('midi_image.png')
