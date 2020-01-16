965f6d669e39db0e9c710b25d67f61758a535c9e
Hongwei (Henry) Zhou <hz1101@nyu.edu>
2020-01-16 16:12:46 -0500

    distretize donzo

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import math
 
 # length in secs
 audio_len = 5
-# resolution in hz
-audio_res = 5
+# how many pixels are in a beat
+pixel_res = 8
 
 # output dimensions = (len*res)^2
 
@@ -17,6 +17,15 @@ midi_data = pretty_midi.PrettyMIDI('midi_downloads/mario.mid')
 num_inst = len(midi_data.instruments)
 
 def discretize(input_time):
+    resoltuion = midi_data.resoltuion # how many ticks in a beat
+    tick_duration = midi_data._tick_scales[0][1] # Get tick duration from the first tempo signature - we assume the tempo to be uniform
+    audio_res = resolution / pixel_res * tick_duration # how long in seconds does a pixel lasts
+
+    print("Resolution: " + str(resolution) + "; tick_duration: " + str(tick_duration) + "; audio_res: " + str(audio_res) )
+
+    return math.floor(input_time / audio_res)
+
+'''
     second = math.floor(input_time) if (math.floor(input_time)<= audio_len) else audio_len
     remainder = input_time % 1
     sector = 0
@@ -27,13 +36,14 @@ def discretize(input_time):
             sector = x
 
     return second * audio_res + sector
+'''
 
 
 print(discretize(1.75))
 
-for inst_index in range (0, num_inst):
+for inst_index  range (0, num_inst):
     instrument = midi_data.instruments[inst_index]
-    print("inst"+str(inst_index))
+    print("inst"+stinr(inst_index))
     for note_index in range (0, len(instrument.notes) - 1):
         if (instrument.notes[note_index].start< audio_len):
             print([instrument.notes[note_index]])
